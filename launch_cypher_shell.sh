6485c408b8ee0e670d8cd5156fd55555f4c026b8
LeSaRDe <Urchone@gmail.com>
2022-01-16 23:25:50 -0500

    modified launch_neo4j.sh, launch_cypher_shell.sh, and added neo4j_singularity.def

--- launch_cypher_shell.sh
+++ launch_cypher_shell.sh
@@ -1,4 +1,26 @@
 #!/bin/sh
 
-#singularity shell /project/biocomplexity/singularity_images/neo4j-enterprise.sif
-singularity shell --writable-tmpfs /project/biocomplexity/singularity_images/neo4j_enterprise_4.3.7.sif
+echo "[RUN CYPHER-SHELL] Starts."
+
+# ARGUMENT CHECKING
+if [ "$#" -lt 1 ]
+  then
+    echo "[RUN CYPHER-SHELL] USAGE:"
+    echo "[RUN CYPHER-SHELL] launch_cypher_shell.sh [NEO4J SIF IMAGE PATH]"
+    echo "[RUN CYPHER-SHELL] Exits."
+    exit -1
+fi
+
+# SET NEO4J SIF IMAGE PATH
+NEO4J_SIF=$2
+if [ ! -f ${NEO4J_SIF} ]
+  then
+    echo "[RUN CYPHER-SHELL] NEO4J SIG IMAGE: ${NEO4J_SIF} dose not exist!"
+    echo "[RUN CYPHER-SHELL] Exits."
+    exit -1
+fi
+echo "[RUN CYPHER-SHELL] Neo4j SIF: ${NEO4J_SIF}"
+
+# RUN CYPHER-SHELL
+echo "[RUN CYPHER-SHELL] Run cypher-shell."
+singularity shell --writable-tmpfs ${NEO4J_SIF}
--- launch_neo4j.sh
+++ launch_neo4j.sh
@@ -1,45 +1,160 @@
 #!/bin/sh
 
-LDB=/project/biocomplexity/mf3jh/neo4j_workspace/data
-CONF=/project/biocomplexity/mf3jh/neo4j_workspace/conf
-PLUGINS=/project/biocomplexity/mf3jh/neo4j_workspace/plugins
-LOGS=/project/biocomplexity/mf3jh/neo4j_workspace/logs
-#IMAGE=/project/biocomplexity/singularity_images/neo4j-enterprise.sif
-IMAGE=/project/biocomplexity/singularity_images/neo4j_enterprise_4.3.7.sif
-echo "Local DB Folder ${LDB}"
+# CAUTION
+# MAKE SURE YOU HAVE CHECKED ALL "TODO" IN THIS FILE BEFORE RUNNING!!!
 
-mkdir -p ${LDB}
-mkdir -p ${CONF}
-mkdir -p ${PLUGINS}
-mkdir -p ${LOGS}
+echo "[RUN NEO4J] Starts."
 
-echo "Neo4j instance name: $SLURM_JOB_ID"
-singularity instance start --env=NEO4J_ACCEPT_LICENSE_AGREEMENT=yes --writable-tmpfs  --bind ${LDB}:/data,${CONF}:/var/lib/neo4j/conf,${PLUGINS}:/var/lib/neo4j/plugins,${LOGS}:/logs ${IMAGE} $SLURM_JOB_ID
-#singularity instance list
-#singularity exec instance://$SLURM_JOB_ID neo4j start
+# ARGUMENT CHECKING
+if [ "$#" -lt 2 ]
+  then
+    echo "[RUN NEO4J] USAGE:"
+    echo "[RUN NEO4J] launch_neo4j.sh [WORK FOLDER] [NEO4J SIF IMAGE PATH] [NEO4J INSTANCE NAME]"
+    echo "[RUN NEO4J] Exits."
+    exit -1
+fi
 
-sleep 10
-ps -aufx | grep neo4j
+# SET WORK FOLDER
+if [ $1 != "*/" ]
+  then
+    WORK_FOLDER=$1/
+  else
+    WORK_FOLDER=$1
+fi
 
-handler()
+if [ ! -d ${WORK_FOLDER} ]
+  then
+    echo "[RUN NEO4J] WORK FOLDER: ${WORK_FOLDER} dose not exist!"
+    echo "[RUN NEO4J] Exits."
+    exit -1
+fi
+echo "[RUN NEO4J] Work Folder: ${WORK_FOLDER}"
+
+# SET NEO4J SIF IMAGE PATH
+NEO4J_SIF=$2
+if [ ! -f ${NEO4J_SIF} ]
+  then
+    echo "[RUN NEO4J] NEO4J SIG IMAGE: ${NEO4J_SIF} dose not exist!"
+    echo "[RUN NEO4J] Exits."
+    exit -1
+fi
+echo "[RUN NEO4J] Neo4j SIF: ${NEO4J_SIF}"
+
+# SET NEO4J INSTANCE NAME
+if [ "$#" -gt 2 ]
+  then
+    NEO4J_INS_NAME=$3
+else
+  NEO4J_INS_NAME=${SLURM_JOB_ID}
+fi
+echo "[RUN NEO4J] Neo4j Singuilarity Instance Name: ${NEO4J_INS_NAME}"
+
+# REMOVE "SHUTDOWN" FROM PREVIOUS RUNS IF ANY
+if [ -f ${WORK_FOLDER}SHUTDOWN ]; then
+  rm ${WORK_FOLDER}SHUTDOWN
+  echo "[RUN NEO4J] Removed SHUTDOWN"
+fi
+
+# SET UP FOLDER EXPOSURE
+# NOTE
+# The "metrics" folder is only available to the Enterprise version.
+
+# CAUTION
+# When exposing the "conf" folder, the default "neo4j.conf" will NOT be
+# forwarded to the outside folder. And thus everything built in the container
+# definition file will NOT be available in this case. So exposing this folder
+# should only be for testing purposes rather than official runs.
+
+# TODO
+# Set the following two lines to "true" to expose "conf" and "metrics"
+# respectively when needed.
+EN_CONF_EXP=false
+EN_METRICS_EXP=false
+
+EXP_DATA=${WORK_FOLDER}data
+EXP_PLUGINS=${WORK_FOLDER}plugins
+EXP_LOGS=${WORK_FOLDER}logs
+EXP_CONF=${WORK_FOLDER}conf
+EXP_METRICS=${WORK_FOLDER}metrics
+
+mkdir -p ${EXP_DATA}
+mkdir -p ${EXP_PLUGINS}
+mkdir -p ${EXP_LOGS}
+if [ ${EN_CONF_EXP} = true ]
+  then
+    mkdir -p ${EXP_CONF}
+fi
+if [ ${EN_METRICS_EXP} = true ]
+  then
+    mkdir -p ${EXP_METRICS}
+fi
+
+chmod -R 777 ${EXP_DATA}
+chmod -R 777 ${EXP_PLUGINS}
+chmod -R 777 ${EXP_LOGS}
+if [ ${EN_CONF_EXP} = true ]
+  then
+    chmod -R 777 ${EXP_CONF}
+fi
+if [ ${EN_METRICS_EXP} = true ]
+  then
+    chmod -R 777 ${EXP_METRICS}
+fi
+
+# TODO
+# These default folders need to be double checked for different versions of Neo4j.
+NEO4J_DEF_FOLDER=/var/lib/neo4j/
+NEO4J_DEF_DATA=${NEO4J_DEF_FOLDER}data
+NEO4J_DEF_LOGS=${NEO4J_DEF_FOLDER}logs
+NEO4J_DEF_PLUGINS=${NEO4J_DEF_FOLDER}plugins
+NEO4J_DEF_CONF=${NEO4J_DEF_FOLDER}conf
+NEO4J_DEF_METRICS=${NEO4J_DEF_FOLDER}metrics
+
+# Folder Exposure Arguments
+FOLDER_EXP_ARG="${EXP_DATA}:${NEO4J_DEF_DATA},${EXP_PLUGINS}:${NEO4J_DEF_PLUGINS},${EXP_LOGS}:${NEO4J_DEF_LOGS}"
+if [ ${EN_CONF_EXP} = true ]
+  then
+    FOLDER_EXP_ARG+="${EXP_CONF}:${NEO4J_DEF_CONF}"
+fi
+if [ ${EN_METRICS_EXP} = true ]
+  then
+    FOLDER_EXP_ARG+="${EXP_METRICS}:${NEO4J_DEF_METRICS}"
+fi
+
+echo "[RUN NEO4J] Data folder: ${NEO4J_DEF_DATA} => ${EXP_DATA}"
+echo "[RUN NEO4J] Plugin folder: ${NEO4J_DEF_PLUGINS} => ${EXP_PLUGINS}"
+echo "[RUN NEO4J] Log folder: ${NEO4J_DEF_LOGS} => ${EXP_LOGS}"
+if [ ${EN_CONF_EXP} = true ]
+  then
+    echo "[RUN NEO4J] Config folder: ${NEO4J_DEF_CONF} => ${EXP_CONF}"
+fi
+if [ ${EN_METRICS_EXP} = true ]
+  then
+    echo "[RUN NEO4J] Metrics folder: ${NEO4J_DEF_METRICS} => ${EXP_METRICS}"
+fi
+
+# RUN NEO4J INSTANCE
+echo "[RUN NEO4J] Starting Neo4j Instance..."
+singularity instance start --env=NEO4J_ACCEPT_LICENSE_AGREEMENT=yes --writable-tmpfs --bind ${FOLDER_EXP_ARG} ${NEO4J_SIF} ${NEO4J_INS_NAME}
+echo "[RUN NEO4J] Neo4j Instance Started."
+
+sigint_handler()
 {
-  echo "Closing Database..."
-  #shutdown the db cleanly
-  echo "Stop DB"
-  # we need to add a stop script in here for neo4j
-  #singularity run --app stop instance://$SLURM_JOB_ID
-
-  # stop the singularity instance
-  echo "Stop Singularity Instance"
-  singularity exec instance://$SLURM_JOB_ID neo4j stop
-  singularity instance stop $SLURM_JOB_ID
-
-  touch ${LDB}/SHUTDOWN
+  echo "[RUN NEO4J] Stopping Neo4j Instance..."
+  singularity instance stop ${NEO4J_INS_NAME}
+  echo "[RUN NEO4J] Neo4j Instance Stopped."
 }
 
-echo "Waiting for DB Completion..."
-while [ ! -f "${LDB}/SHUTDOWN" ] ;do
-  sleep 10
-done;
+echo "[RUN NEO4J] Waiting for Neo4j Instance Completion..."
+while true
+do
+  if [ -f "${WORK_FOLDER}/SHUTDOWN" ]
+    then
+      sigint_handler
+      exit 0
+    else
+      sleep 5
+  fi
+done
 
-trap handler SIGINT
+trap sigint_handler INT
